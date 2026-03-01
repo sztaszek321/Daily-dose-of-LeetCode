b481bc32beeeaf26f9963a3ac18d19dc102d22ee
Staszek <sztaszek321@gmail.com>
2026-03-01 15:57:06 +0100

    16th day with leetcode (01.03.2026)

---
+++ 1689 - Partitioning Into Minimum Number Of Deci-Binary Numbers/results.md
@@ -0,0 +1,8 @@
+### Passed
+
+Code passed each of the 97 testcases
+
+runtime: 0 ms, beats 100%  
+memory 20.24 MB, beats 21.05%
+
+Probably the easiest one so far, glad to find the solution that beats 100% in runtime (only 8.68%).
---
+++ 1689 - Partitioning Into Minimum Number Of Deci-Binary Numbers/solution.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        if "9" in n:
+            return 9
+        if "8" in n:
+            return 8
+        if "7" in n:
+            return 7
+        if "6" in n:
+            return 6
+        if "5" in n:
+            return 5
+        if "4" in n:
+            return 4
+        if "3" in n:
+            return 3
+        if "2" in n:
+            return 2
+        if "1" in n:
+            return 1
+        return 0
+
+t1 = "32"
+t2 = "82734"
+t3 = "27346209830709182346"
+
+sol = Solution()
+print(sol.minPartitions(t2))
+
+### second solution
+"""class Solution:
+    def minPartitions(self, n: str) -> int:
+        return int(max(n))"""
